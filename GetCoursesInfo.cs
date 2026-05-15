b3d49585a6f3456d35d0a46e9f708b949c4420b0
MasterBuilderMAC <55816183+MasterBuilderMAC@users.noreply.github.com>
2026-05-14 17:09:18 -0700

    Update GetCoursesInfo.cs

--- GetCoursesInfo.cs
+++ GetCoursesInfo.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,16 +13,20 @@ namespace CustomMaps
 {
     static public class GetCoursesInfo
     {
-        
+        //Create the course, the custom tab at the top
         public static CourseData getCourse()
         {
+            
             string imagePath = "";
             string modFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             CourseData courseData = ScriptableObject.CreateInstance<CourseData>();
 
+            courseData.name = "Custom Courses";
+
             Traverse tCourse = Traverse.Create(courseData);
 
-            LocalizedString localizedCourseName = new LocalizedString("Custom Courses", "Custom Courses");
+            LocalizedString localizedCourseName = new LocalizedString("UI", "Mac.CustomMaps");
+            Plugin.CustomLocalizedStrings["Mac.CustomMaps"] = "Custom Holes";
             tCourse.Property("LocalizedName").SetValue(localizedCourseName);
 
             
@@ -44,22 +48,24 @@ namespace CustomMaps
 
             tCourse.Property("WindAmbienceType").SetValue(WindManager.WindAudioAmbienceType.Default);
 
-            //TODO: post processing (optional?)
+            //no post processing, null in all courses
 
             tCourse.Field("includeAllHoles").SetValue(false);
             tCourse.Field("difficultyCourse").SetValue(false);
 
 
             //TODO: Holes. For now, 1 for proof of concept
-            HoleData holeData = new HoleData();
+            HoleData holeData = ScriptableObject.CreateInstance<HoleData>();
 
             Traverse tHole = Traverse.Create(holeData);
 
-            LocalizedString localizedHoleName = new LocalizedString("Custom Hole", "Custom Hole");
+            LocalizedString localizedHoleName = new LocalizedString("UI", "Mac.TestMapName");
+            Plugin.CustomLocalizedStrings["Mac.TestMapName"] = "Not Hilltops";
             tHole.Property("LocalizedName").SetValue(localizedHoleName);
 
-            //TODO: Scene reference
-            SceneReference sceneReference = new SceneReference(); //add valid scene # to args
+            holeData.name = "Not Hilltops";
+
+            SceneReference sceneReference = new SceneReference(Plugin.SceneGuids[0]); //scene guid doesnt matter, injected from patches
             tHole.Property("Scene").SetValue(sceneReference);
 
             tHole.Property("Par").SetValue(12);
@@ -72,9 +78,24 @@ namespace CustomMaps
             tHole.Property("ScreenshotsThumbnail").SetValue(sprites);
 
             //TODO: Event reference music event
+            var musicGuid = new FMOD.GUID
+            {
+                Data1 = -55891042,  
+                Data2 = 1301584857,
+                Data3 = 1030450600,
+                Data4 = 1380137089
+            };
+
+            var musicEvent = new FMODUnity.EventReference
+            {Guid = musicGuid};
+            tHole.Property("MusicEvent").SetValue(musicEvent);
 
             HoleData[] allHoles = [holeData];
             tCourse.Property("Holes").SetValue(allHoles);
+
+
+
+            Plugin.CustomCourse = courseData;
 
             return courseData;
         }
